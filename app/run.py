b3f264e4b6dd9bf4bc240097955e5ee336cb455c
Ian <iankruze60@gmail.com>
2025-07-16 10:53:25 +0300

    (backend): TSK-02 Add Flask run script (run.py) in app/ directory

---
+++ app/run.py
@@ -0,0 +1,16 @@
+from app import app, db
+from flask_migrate import Migrate
+# from models.user import User
+# from models.club import Club
+# from models.movie import Movie
+# from models.post import Post
+# from models.review import Review
+# from models.club_member import ClubMember
+# from models.follower import Follower
+
+
+
+migrate = Migrate(app, db)
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+        
