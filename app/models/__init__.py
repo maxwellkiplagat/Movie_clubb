79b5dffaf0a13631ac055247c808ab99fb7e91da
Ian <iankruze60@gmail.com>
2025-07-16 11:10:39 +0300

    (backend): TSK-02 Initialize models package in app/models/

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+from app import db
+# from .user import User
+# from .club import Club
+# from .movie import Movie
+# from .post import Post
+# from .review import Review
+# from .club_member import ClubMember
+# from .follower import Follower
+
+class BaseModelMixin(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
