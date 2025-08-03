3a304c556cf99a224f8be86385724145304c6399
MAXWELL <maxwellkiplagat0@gmail.com>
2025-08-03 15:32:13 +0300

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -24,7 +24,7 @@ mail = Mail() # NEW: Initialize Flask-Mail
 def create_app():
     # Create and configure the Flask application
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
