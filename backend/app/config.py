9d42a6d5689fa71bf2f037c3435ebb3c5fd1b6f0
MAXWELL <maxwellkiplagat0@gmail.com>
2025-08-03 15:52:05 +0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,5 @@
 import os
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///movies.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
